f400648c3299605a2973f650869e442f41cb094c
Dorian Buck <dorianbuck@gmail.com>
2021-11-08 21:54:21 +0100

    adds dependencies, react-router-dom and mui/materials, alse creates the component Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <h1>Hello fika world!</h1>
-    </div>
+    </BrowserRouter>
   );
 };
 
