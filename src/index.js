08777a04ab442a946f6b779655a1ad64f6987f2e
Fraseer <Fraser.Hughes@btinternet.com>
2021-11-09 23:02:13 +0100

    adds dark theme

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import App from "./App";
 import axios from "axios";
 import store from "./state/store/configureStore";
 import { Provider } from "react-redux";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { amber } from "@mui/material/colors";
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   axios.defaults.baseURL = "http://localhost:3000";
@@ -11,10 +14,25 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   axios.defaults.baseURL = "https://fika-together.herokuapp.com";
 }
 
+const getDesignTokens = (mode) => ({
+  palette: {
+    mode,
+    primary: {
+      ...amber,
+      ...(mode === "dark"),
+    },
+  },
+});
+
+const darkModeTheme = createTheme(getDesignTokens("dark"));
+
 window.store = store;
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider theme={darkModeTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </Provider>,
   document.getElementById("root")
 );
