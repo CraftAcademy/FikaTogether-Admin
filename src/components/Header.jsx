c2497b5f1088b84ff276fbb211704a98397b6f99
Dorian Buck <dorianbuck@gmail.com>
2021-11-09 12:39:26 +0100

    adds dependency react-response and implement desktop/mobile view for navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 import { Box, Paper } from "@mui/material";
 import { BottomNavigation, BottomNavigationAction } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
@@ -7,37 +8,75 @@ import HelpIcon from "@mui/icons-material/Help";
 import EventIcon from "@mui/icons-material/Event";
 
 const Header = () => {
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 500px)" });
+
   return (
-    <Box sx={{ pb: 7 }}>
-      <Paper
-        sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
-        elevation={3}
-      >
-        <BottomNavigation showLabels>
-          <BottomNavigationAction
-            component={Link}
-            to="/"
-            data-cy="events-btn"
-            icon={<HomeIcon />}
-            label="Events"
-          />
-          <BottomNavigationAction
-            component={Link}
-            to="/departments"
-            data-cy="departments-btn"
-            label="Departments"
-            icon={<EventIcon />}
-          />
-          <BottomNavigationAction
-            component={Link}
-            to="/about"
-            data-cy="about-btn"
-            icon={<HelpIcon />}
-            label="About"
-          />
-        </BottomNavigation>
-      </Paper>
-    </Box>
+    <>
+      {isTabletOrMobile && (
+        <Box sx={{ pb: 7 }}>
+          <Paper
+            sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+            elevation={3}
+          >
+            <BottomNavigation showLabels>
+              <BottomNavigationAction
+                component={Link}
+                to="/"
+                data-cy="events-btn"
+                icon={<HomeIcon />}
+                label="Events"
+              />
+              <BottomNavigationAction
+                component={Link}
+                to="/departments"
+                data-cy="departments-btn"
+                label="Departments"
+                icon={<EventIcon />}
+              />
+              <BottomNavigationAction
+                component={Link}
+                to="/about"
+                data-cy="about-btn"
+                icon={<HelpIcon />}
+                label="About"
+              />
+            </BottomNavigation>
+          </Paper>
+        </Box>
+      )}
+      {!isTabletOrMobile && (
+        <Box sx={{ pb: 7 }}>
+          <Paper
+            sx={{ position: "fixed", top: 0, left: 0, right: 0 }}
+            elevation={3}
+          >
+            <BottomNavigation showLabels>
+              <BottomNavigationAction
+                component={Link}
+                to="/"
+                data-cy="events-btn"
+                icon={<HomeIcon />}
+                label="Events"
+              />
+              <BottomNavigationAction
+                component={Link}
+                to="/departments"
+                data-cy="departments-btn"
+                label="Departments"
+                icon={<EventIcon />}
+              />
+              <BottomNavigationAction
+                component={Link}
+                to="/about"
+                data-cy="about-btn"
+                icon={<HelpIcon />}
+                label="About"
+              />
+            </BottomNavigation>
+          </Paper>
+        </Box>
+      )}
+    </>
   );
 };
 
