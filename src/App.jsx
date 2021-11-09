153f17abab9d06935e68db80ec9d6a6035a4ad5f
Fraseer <Fraser.Hughes@btinternet.com>
2021-11-09 23:36:37 +0100

    adds button to switch between light and dark

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useMemo, createContext } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Departments from "./components/Departments";
@@ -6,33 +6,59 @@ import Events from "./components/Events";
 import About from "./components/About";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
-import { amber } from "@mui/material/colors";
+import IconButton from "@mui/material/IconButton";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+
+const ColorModeContext = createContext({ toggleColorMode: () => {} });
 
 const App = () => {
-  const getDesignTokens = (mode) => ({
-    palette: {
-      mode,
-      primary: {
-        ...amber,
-        ...(mode === "dark"),
+  const [mode, setMode] = useState("light");
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
       },
-    },
-  });
+    }),
+    []
+  );
 
-  const darkModeTheme = createTheme(getDesignTokens("dark"));
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  );
+
   return (
-    <ThemeProvider theme={darkModeTheme}>
-      <CssBaseline />
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
 
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<Events />} />
-          <Route path="/departments" element={<Departments />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<Events />} />
+            <Route path="/departments" element={<Departments />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+          <IconButton
+            sx={{ ml: 1 }}
+            onClick={colorMode.toggleColorMode}
+            color="inherit"
+          >
+            {theme.palette.mode === "dark" ? (
+              <Brightness7Icon />
+            ) : (
+              <Brightness4Icon />
+            )}
+          </IconButton>
+        </BrowserRouter>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 };
 
