278e60c006dbd1def2e3691ffaa0099d85ab02ab
Fraseer <Fraser.Hughes@btinternet.com>
2021-11-10 23:42:46 +0100

    reformats test file

--- cypress/integration/userCanSeeListOfDepartments.feature.js
+++ cypress/integration/userCanSeeListOfDepartments.feature.js
@@ -4,14 +4,17 @@ describe("Admin can see a list of departments", () => {
       fixture: "fikaList.json",
       statusCode: 200,
     });
-    cy.intercept("GET", "**/api/departments**", {
-      fixture: "departmentList.json",
-      statusCode: 200,
-    });
-    cy.visit("/");
   });
 
   describe("Departments are displayed in the data table", () => {
+    before(() => {
+      cy.intercept("GET", "**/api/departments**", {
+        fixture: "departmentList.json",
+        statusCode: 200,
+      });
+      cy.visit("/");
+    });
+    
     it("is expected that the fika table is visible", () => {
       cy.get("[data-cy=departments-btn]").click();
       cy.get("[data-cy=department-table]")
@@ -23,25 +26,21 @@ describe("Admin can see a list of departments", () => {
         });
     });
   });
-});
 
-describe("Admin does not see a list of departments", () => {
-  beforeEach(() => {
-    cy.intercept("GET", "**/api/fikas**", {
-      fixture: "fikaList.json",
-      statusCode: 200,
+  describe("Admin does not see a list of departments", () => {
+    before(() => {
+      cy.intercept("GET", "**/api/departments**", {
+        body: { message: "There are no departments in the database" },
+        statusCode: 404,
+      });
+      cy.visit("/");
     });
-    cy.intercept("GET", "**/api/departments**", {
-      body: { message: "There are no departments in the database" },
-      statusCode: 404,
-    });
-    cy.visit("/");
-  });
 
-  it("is expected to unsuccessfully display api response from departments", () => {
-    cy.get("[data-cy=departments-btn]").click();
-    cy.get("[data-cy=department-table]").within(() => {
-      cy.contains("No rows").should("be.visible");
+    it("is expected to unsuccessfully display api response from departments", () => {
+      cy.get("[data-cy=departments-btn]").click();
+      cy.get("[data-cy=department-table]").within(() => {
+        cy.contains("No rows").should("be.visible");
+      });
     });
   });
 });
