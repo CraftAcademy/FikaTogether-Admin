627d4c2b37545778c90997a8e0ba3f896b32d7d5
Dorian Buck <dorianbuck@gmail.com>
2021-11-10 18:13:33 +0100

    adds sad path for test

--- cypress/integration/adminCanSeeListOfDepartments.feature.js
+++ cypress/integration/adminCanSeeListOfDepartments.feature.js
@@ -14,12 +14,38 @@ describe("Admin can see a list of departments", () => {
   describe("Departments are displayed in the data table", () => {
     it("is expected that the fika table is visible", () => {
       cy.get("[data-cy=departments-btn]").click();
-      cy.get("[data-cy=department-table]").should("be.visible");
+      cy.get("[data-cy=department-table]")
+        .should("be.visible")
+        .within(() => {
+          cy.contains("Department").should("be.visible");
+          cy.contains("Average Fika Score").should("be.visible");
+          cy.contains("HR")
+            .should("be.visible")
+            .next()
+            .contains(4)
+            .should("be.visible");
+        });
     });
+  });
+});
 
-    it('is expected to display Department and Average Fika Score columns', () => {
-      
-      
+describe("Admin does not see a list of departments", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "**/api/fikas**", {
+      fixture: "fikaList.json",
+      statusCode: 200,
+    });
+    cy.intercept("GET", "**api/departments**", {
+      fixture: "",
+      statusCode: 422,
+    });
+    cy.visit("/");
+  });
+
+  it("is expected to unsuccessfully display api response from departments", () => {
+    cy.get("[data-cy=departments-btn]").click()
+    cy.get("[data-cy=department-table]").within(() => {
+      cy.contains("No rows").should("be.visible");
     });
   });
 });
