6fab81cba21d8ea348d4f0887dec8a847a1eb11a
Dorian Buck <dorianbuck@gmail.com>
2021-11-13 17:09:31 +0100

    adds the sad path to test for login feature

--- cypress/integration/userCanSignIntoTheSite.feature.js
+++ cypress/integration/userCanSignIntoTheSite.feature.js
@@ -1,15 +1,15 @@
 describe("A login view render on site load", () => {
   beforeEach(() => {
-    cy.intercept("POST", "**api/auth/sign_in", {
+    cy.intercept("POST", "**auth/sign_in", {
       fixture: "authenticationSuccess.json",
       headers: { uid: "user@email.com" },
     });
-    cy.intercept("GET", "**api/auth/validate_token**", {
+    cy.intercept("GET", "**auth/validate_token**", {
       fixture: "authenticationSuccess.json",
     });
-
     cy.visit("/");
   });
+
   it("is expected to have two input fields and a submit button", () => {
     cy.get("[data-cy=sign-in-form]").children().should("have.length", 3);
   });
@@ -30,18 +30,20 @@ describe("A login view render on site load", () => {
 
   describe("when sign in is not successful", () => {
     beforeEach(() => {
-      cy.intercept("POST", "**api/auth/sign_in", {
+      cy.intercept("POST", "**auth/sign_in", {
         statusCode: 401,
         fixture: "authenticationFailure.json",
       });
-      cy.intercept("GET", "**api/auth/validate_token**", {
+      cy.intercept("GET", "**auth/validate_token**", {
         statusCode: 401,
         fixture: "authenticationFailure.json",
       });
+      cy.visit("/");
       cy.get("[data-cy=email-input]").type("user@email.com");
       cy.get("[data-cy=password-input]").type("wrong password");
       cy.get("[data-cy=btn-login]").click();
     });
+    
     it("is expected to display an error message", () => {
       cy.get("[data-cy=sign-in-toast]").within(() => {
         cy.contains("Invalid login credentials. Please try again.").should("be.visible")
