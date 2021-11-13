3aeadf9350a4da32b0ae90baef0312cd2391550b
Dorian Buck <dorianbuck@gmail.com>
2021-11-13 13:08:19 +0100

    adds login functionality with axios to login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,24 +4,34 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { ToastContainer, toast } from "react-toastify";
 import { useDispatch } from "react-redux";
+import auth from "../modules/auth";
 
 const Login = () => {
   const dispatch = useDispatch();
-  const onSubmit = () => {
-    toast.success("Login Successful", {
-      onClose: () =>
-        dispatch({
-          type: "SET_CURRENT_USER",
-          payload: true,
-        }),
-    });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    try {
+      const response = await auth.signIn(email, password);
+
+      toast.success("Login Successful", {
+        onClose: () =>
+          dispatch({
+            type: "SET_CURRENT_USER",
+            payload: response.data,
+          }),
+      });
+    } catch (error) {
+      toast.error(error.response.data.errors[0]);
+    }
   };
 
   return (
     <>
       <Box
-        data-cy="sign-in-form"
-        component="form"
         sx={{
           "& > :not(style)": { m: 1, width: "25ch" },
           textAlign: "center",
@@ -29,28 +39,32 @@ const Login = () => {
         noValidate
         autoComplete="off"
       >
-        <TextField
-          data-cy="email-input"
-          id="outlined-basic"
-          label="Email"
-          variant="outlined"
-          margin="dense"
-        />
-        <TextField
-          data-cy="password-input"
-          id="outlined-basic"
-          label="Password"
-          variant="outlined"
-          margin="dense"
-        />
-        <Button
-          data-cy="btn-login"
-          onClick={onSubmit}
-          variant="contained"
-          margin="dense"
-        >
-          Sign In
-        </Button>
+        <form onSubmit={handleSubmit} data-cy="sign-in-form">
+          <TextField
+            data-cy="email-input"
+            defaultValue="email"
+            id="email"
+            label="Email"
+            variant="outlined"
+            margin="dense"
+          />
+          <TextField
+            data-cy="password-input"
+            defaultValue="password"
+            id="password"
+            label="Password"
+            variant="outlined"
+            margin="dense"
+          />
+          <Button
+            data-cy="btn-login"
+            type="submit"
+            variant="contained"
+            margin="dense"
+          >
+            Sign In
+          </Button>
+        </form>
       </Box>
       <div data-cy="sign-in-toast">
         <ToastContainer
