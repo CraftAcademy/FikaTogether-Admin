ebea6f1373e7c3ce1e159241b3d1bedd1d535e0b
maxarvid <max.arvid.anderson@gmail.com>
2021-11-16 19:33:53 +0100

    WIP refactors mapping of participants by department, passes open/close functions through props down to modal child

--- src/components/AddParticipantModal.jsx
+++ src/components/AddParticipantModal.jsx
@@ -1,125 +1,132 @@
 import React, { useState } from "react";
 import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
+// import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormLabel from "@mui/material/FormLabel";
-import { useTranslation } from "react-i18next";
-import DesktopDatePicker from "@mui/lab/DesktopDatePicker";
-import DateAdapter from "@mui/lab/AdapterDayjs";
+// import Radio from "@mui/material/Radio";
+// import RadioGroup from "@mui/material/RadioGroup";
+// import FormControlLabel from "@mui/material/FormControlLabel";
+// import FormLabel from "@mui/material/FormLabel";
+// import { useTranslation } from "react-i18next";
+// import DesktopDatePicker from "@mui/lab/DesktopDatePicker";
+// import DateAdapter from "@mui/lab/AdapterDayjs";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
-import SeniorityMenu from "./SeniorityMenu";
+import { Modal } from "@mui/material";
+// import SeniorityMenu from "./SeniorityMenu";
 
-const AddParticipantModal = () => {
-  const { t } = useTranslation();
+const AddParticipantModal = (props) => {
+  // const { t } = useTranslation();
   // const [dateValue, setDateValue] = useState();
-  const [name, setName] = useState()
-  
-  // const handleChange = (event) => {    
-  //   setName(event.target.value);    
+  // const [name, setName] = useState()
+
+  // const handleChange = (event) => {
+  //   setName(event.target.value);
   // };
-  
-  const handleSubmit = async (event) => {
-    debugger;
-    event.preventDefault();
 
-    // const name = form.name.value;
-    const form = event.target;
-    // const email = form.email.value;
-    // const seniority = form.seniority.value;
-    // const date = dateValue;
+  // const handleSubmit = async (event) => {
+  //   debugger;
+  //   event.preventDefault();
 
-    // try {
-    //   const response = await post(name, email, seniority, date);
-  
-    console.log(form);
-  };
+  //   // const name = form.name.value;
+  //   const form = event.target;
+  //   // const email = form.email.value;
+  //   // const seniority = form.seniority.value;
+  //   // const date = dateValue;
 
-  const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 400,
-    bgcolor: "background.paper",
-    border: "2px solid #000",
-    boxShadow: 24,
-    p: 4,
-  };
+  //   // try {
+  //   //   const response = await post(name, email, seniority, date);
+
+  //   console.log(form);
+  // };
+
+  // const style = {
+  //   position: "absolute",
+  //   top: "50%",
+  //   left: "50%",
+  //   transform: "translate(-50%, -50%)",
+  //   width: 400,
+  //   bgcolor: "background.paper",
+  //   border: "2px solid #000",
+  //   boxShadow: 24,
+  //   p: 4,
+  // };
 
   return (
-    <LocalizationProvider dateAdapter={DateAdapter}>
-      <Box
-        data-cy="add-participant-form"
-        sx={style}
-        component="form"
-        onSubmit={handleSubmit}
-      >
-        <TextField
-          data-cy="name-input"
-          id="name"
-          label={t("inputNameLabel")}
-          variant="outlined"
-          margin="dense"
-          fullWidth="true"
-          value={name}
-          // onChange={handleChange}
-        />
-        <TextField
-          data-cy="email-input"
-          id="email"
-          label={t("email")}
-          variant="outlined"
-          margin="dense"
-          fullWidth="true"
-        />
-        <div data-cy="start-date-input">
-          {/* <DesktopDatePicker
-            label={t("InputStartDate")}
-            // inputFormat="yyyy/mm/dd"
-            // value={dateValue}
-            // onChange={handleChange}
-            renderInput={(params) => <TextField {...params} />}
-          /> */}
-        </div>
-        <FormLabel component="legend">{t("Management")}</FormLabel>
-        <RadioGroup
-          row
-          aria-label={t("selectManagementLabel")}
-          name="row-radio-buttons-group"
-          defaultValue="Non Management"
-          data-cy="management"
-        >
-          <FormControlLabel
-            value="Management"
-            control={<Radio />}
-            label={t("Management")}
-          />
-          <FormControlLabel
-            value="Non Management"
-            control={<Radio />}
-            label={t("nonManagement")}
-          />
-        </RadioGroup>
-        <SeniorityMenu />
-        <Button
-          data-cy="add-btn"
-          type="submit"
-          variant="contained"
-          margin="dense"
-          fullWidth="true"
-          sx={{
-            backgroundColor: "#4C9074",
-            m: 2,
-          }}
-        >
-          {t("addsParticipantBtn")}
-        </Button>
-      </Box>
-    </LocalizationProvider>
-  );
+    <Modal aria-labelledby="modal-modal-title"
+    aria-describedby="modal-modal-description">
+
+    </Modal>
+  
+  
+    );
 };
 
 export default AddParticipantModal;
+
+// // <LocalizationProvider dateAdapter={DateAdapter}>
+// <Box
+//   data-cy="add-participant-form"
+//   sx={style}
+//   component="form"
+//   onSubmit={handleSubmit}
+// >
+//   <TextField
+//     data-cy="name-input"
+//     // id="name"
+//     // label={t("inputNameLabel")}
+//     variant="outlined"
+//     margin="dense"
+//     // fullWidth="true"
+//     // onChange={handleChange}
+//   />
+//   <TextField
+//     data-cy="email-input"
+//     id="email"
+//     label={t("email")}
+//     variant="outlined"
+//     margin="dense"
+//     fullWidth="true"
+//   />
+//   <div data-cy="start-date-input">
+//     <DesktopDatePicker
+//       label={t("InputStartDate")}
+//       // inputFormat="yyyy/mm/dd"
+//       // value={dateValue}
+//       // onChange={handleChange}
+//       renderInput={(params) => <TextField {...params} />}
+//     />
+//   </div>
+//   <FormLabel component="legend">{t("Management")}</FormLabel>
+//   <RadioGroup
+//     row
+//     aria-label={t("selectManagementLabel")}
+//     name="row-radio-buttons-group"
+//     defaultValue="Non Management"
+//     data-cy="management"
+//   >
+//     <FormControlLabel
+//       value="Management"
+//       control={<Radio />}
+//       label={t("Management")}
+//     />
+//     <FormControlLabel
+//       value="Non Management"
+//       control={<Radio />}
+//       label={t("nonManagement")}
+//     />
+//   </RadioGroup>
+//   <SeniorityMenu />
+//   <Button
+//     // data-cy="add-btn"
+//     // type="submit"
+//     variant="contained"
+//     margin="dense"
+//     // fullWidth="true"
+//     sx={{
+//       backgroundColor: "#4C9074",
+//       m: 2,
+//     }}
+//   >
+//     {t("addsParticipantBtn")}
+//   </Button>
+// </Box>
+// // </LocalizationProvider>
--- src/components/ParticipantList.jsx
+++ src/components/ParticipantList.jsx
@@ -5,7 +5,6 @@ import Container from "@mui/material/Container";
 import { makeStyles } from "@mui/styles";
 import { LoadingButton } from "@mui/lab";
 import AddParticipantModal from "./AddParticipantModal";
-import Modal from "@mui/material/Modal";
 
 const ParticipantList = () => {
   const { participantList, departments } = useSelector((state) => state);
@@ -32,15 +31,15 @@ const ParticipantList = () => {
     },
   ];
 
-  let rows = departments[participantList.id - 1].participants.map(
-    (participant) => {
+  let rows = departments
+    .find((department) => department.id === participantList.id)
+    .participants.map((participant) => {
       return {
         id: participant.id,
         name: participant.name,
         score: participant.fika_score,
       };
-    }
-  );
+    });
 
   const useStyles = makeStyles({
     root: {
@@ -64,24 +63,15 @@ const ParticipantList = () => {
         type="link"
         variant="contained"
         margin="dense"
-        // loading={loading}
         sx={{
           backgroundColor: "#4C9074",
-          m: 5
+          m: 5,
         }}
         onClick={handleOpen}
       >
         Manage Participant
       </LoadingButton>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <AddParticipantModal />
-      </Modal>
-
+      <AddParticipantModal open={open} onCLose={handleClose} />
       <div
         style={{ width: "100%", height: 400 }}
         data-cy="participant-table"
